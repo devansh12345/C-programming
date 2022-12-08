3bceb772fc15ecd7acc65c286520316a01532619
Devansh <devanshgautam8@gmail.com>
2022-12-08 16:36:47 +0530

    Basic input/output
    Signed-off-by: Devansh <devanshgautam8@gmail.com>

---
+++ input_output.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    // note  cout and cin both are object not function so they will return nothing
+    cout<< "enter the value of x"<< endl;
+    cin >> x;
+    cout<< "enter the value of x="<< x << endl;
+    cin >> x;
+    cout<< " the value of x="<< x << endl;
+
+    
+    
+}
