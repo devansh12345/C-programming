076bbe6e7031c4a1ceb0e0c7fe4c95588b314d41
Devansh <devanshgautam8@gmail.com>
2022-12-09 12:44:26 +0530

    flags
    Signed-off-by: Devansh <devanshgautam8@gmail.com>

---
+++ flags.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int x;
+cout<<"enter the value of x"<<endl;
+cin>>x;
+cout<<"x in decimal"<<dec<<x;
+cout<<"x in decimal"<<oct<<x;
+
+return 0;
+}
